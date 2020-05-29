d1222640f76f3ca487d6725eda971df2ede9b2b6
FlameYagami <394198188@qq.com>
2020-05-29 16:05:38 +0800

    Add :BaseItemVm

--- component/src/main/java/com/mvvm/component/uc/recyclerview/multi/MultiItemVm.kt
+++ component/src/main/java/com/mvvm/component/uc/recyclerview/multi/MultiItemVm.kt
@@ -1,20 +1,10 @@
 package com.mvvm.component.uc.recyclerview.multi
 
-import androidx.databinding.ObservableBoolean
-import androidx.databinding.ObservableField
-import com.mvvm.component.uc.recyclerview.LayoutManagerHelper
-import com.mvvm.component.uc.widget.RefreshStatus
-import com.mvvm.component.view.BaseVm
+import com.mvvm.component.uc.recyclerview.BaseItemVm
 
 /**
  * Created by FlameYagami on 2018/1/3.
  */
-abstract class MultiItemVm<R, T> protected constructor() : BaseVm() {
-
-    var isFooterEnabled = ObservableBoolean(false)
-    var refreshStatus = ObservableField(RefreshStatus.NA)
-
-    abstract val layoutManagerHelper: LayoutManagerHelper
-
-    open var adapter: MultiItemAdapter<R, T> = MultiItemAdapter()
+abstract class MultiItemVm<R, T> : BaseItemVm() {
+    var adapter: MultiItemAdapter<R, T> = MultiItemAdapter()
 }
--- component/src/main/java/com/mvvm/component/uc/recyclerview/sgl/SglItemVm.kt
+++ component/src/main/java/com/mvvm/component/uc/recyclerview/sgl/SglItemVm.kt
@@ -1,24 +1,10 @@
 package com.mvvm.component.uc.recyclerview.sgl
 
-import androidx.databinding.ObservableBoolean
-import androidx.databinding.ObservableField
-import com.mvvm.component.uc.recyclerview.LayoutManagerHelper
-import com.mvvm.component.uc.widget.RefreshStatus
-import com.mvvm.component.view.BaseVm
+import com.mvvm.component.uc.recyclerview.BaseItemVm
 
 /**
  * Created by FlameYagami on 2018/1/3.
  */
-abstract class SglItemVm<T> protected constructor() : BaseVm() {
-
-    var isFooterEnabled = ObservableBoolean(false)
-    var refreshStatus = ObservableField(RefreshStatus.NA)
-
-    abstract val layoutManagerHelper: LayoutManagerHelper
-
-    fun notifyRefreshStatus(){
-        refreshStatus.set(RefreshStatus.NA)
-    }
-
-    open var adapter: SglItemAdapter<T> = SglItemAdapter()
+abstract class SglItemVm<T> : BaseItemVm() {
+    var adapter: SglItemAdapter<T> = SglItemAdapter()
 }
