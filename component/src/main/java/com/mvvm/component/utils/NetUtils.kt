89f5a310e66db95671b865d65fc1442b45412d7a
FlameYagami <394198188@qq.com>
2020-08-19 10:00:46 +0800

    Update : simple NetUtils.kt method

--- component/src/main/java/com/mvvm/component/utils/NetUtils.kt
+++ component/src/main/java/com/mvvm/component/utils/NetUtils.kt
@@ -1,13 +1,7 @@
 package com.mvvm.component.utils
 
-import android.Manifest.permission.ACCESS_NETWORK_STATE
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.NetworkInfo
-import android.os.Build
-import android.telephony.TelephonyManager
-import androidx.annotation.RequiresPermission
 import com.mvvm.component.BaseApplication.Companion.context
 
 
@@ -18,93 +12,26 @@ import com.mvvm.component.BaseApplication.Companion.context
 object NetUtils {
 
     enum class NetworkType {
-        NETWORK_ETHERNET,
+        NETWORK_NONE,
         NETWORK_WIFI,
-        NETWORK_4G,
-        NETWORK_3G,
-        NETWORK_2G,
-        NETWORK_UNKNOWN,
-        NETWORK_NO
+        NETWORK_MOBILE,
+        NETWORK_UNKNOWN
     }
 
     fun isNetworkConnect(): Boolean {
-        var result = false
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
-            if (capabilities != null) {
-                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    result = true
-                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                    result = true
-                }
-            }
-        } else {
-            val activeNetwork = cm.activeNetworkInfo
-            if (activeNetwork != null) {
-                // connected to the internet
-                if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
-                    result = true
-                } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
-                    result = true
-                }
-            }
-        }
-        return result
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
-    @RequiresPermission(ACCESS_NETWORK_STATE)
-    private fun isEthernet(): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val info = cm.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET) ?: return false
-        val state = info.state ?: return false
-        return state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING
-    }
-
-    @RequiresPermission(ACCESS_NETWORK_STATE)
-    private fun getActiveNetworkInfo(): NetworkInfo? {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return cm.activeNetworkInfo
-    }
-
-    @RequiresPermission(ACCESS_NETWORK_STATE)
     fun getNetworkType(): NetworkType {
-        if (isEthernet()) {
-            return NetUtils.NetworkType.NETWORK_ETHERNET
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetInfo = connectivityManager.activeNetworkInfo
+        return when {
+            activeNetInfo == null -> NetworkType.NETWORK_NONE
+            activeNetInfo.type == ConnectivityManager.TYPE_WIFI -> NetworkType.NETWORK_WIFI
+            activeNetInfo.type == ConnectivityManager.TYPE_MOBILE -> NetworkType.NETWORK_MOBILE
+            else -> NetworkType.NETWORK_UNKNOWN
         }
-        val info = getActiveNetworkInfo()
-        if (info != null && info.isAvailable) {
-            if (info.type == ConnectivityManager.TYPE_WIFI) {
-                return NetUtils.NetworkType.NETWORK_WIFI
-            } else if (info.type == ConnectivityManager.TYPE_MOBILE) {
-                when (info.subtype) {
-                    TelephonyManager.NETWORK_TYPE_GSM,
-                    TelephonyManager.NETWORK_TYPE_GPRS,
-                    TelephonyManager.NETWORK_TYPE_CDMA,
-                    TelephonyManager.NETWORK_TYPE_EDGE,
-                    TelephonyManager.NETWORK_TYPE_1xRTT,
-                    TelephonyManager.NETWORK_TYPE_IDEN -> return NetUtils.NetworkType.NETWORK_2G
-                    TelephonyManager.NETWORK_TYPE_TD_SCDMA,
-                    TelephonyManager.NETWORK_TYPE_EVDO_A,
-                    TelephonyManager.NETWORK_TYPE_UMTS,
-                    TelephonyManager.NETWORK_TYPE_EVDO_0,
-                    TelephonyManager.NETWORK_TYPE_HSDPA,
-                    TelephonyManager.NETWORK_TYPE_HSUPA,
-                    TelephonyManager.NETWORK_TYPE_HSPA,
-                    TelephonyManager.NETWORK_TYPE_EVDO_B,
-                    TelephonyManager.NETWORK_TYPE_EHRPD,
-                    TelephonyManager.NETWORK_TYPE_HSPAP -> return NetUtils.NetworkType.NETWORK_3G
-                    TelephonyManager.NETWORK_TYPE_IWLAN,
-                    TelephonyManager.NETWORK_TYPE_LTE -> return NetUtils.NetworkType.NETWORK_4G
-                    else -> {
-                        val subtypeName = info.subtypeName
-                        if (subtypeName.equals("TD-SCDMA", ignoreCase = true) || subtypeName.equals("WCDMA", ignoreCase = true) || subtypeName.equals("CDMA2000", ignoreCase = true)) {
-                            return NetUtils.NetworkType.NETWORK_3G
-                        }
-                    }
-                }
-            }
-        }
-        return NetUtils.NetworkType.NETWORK_UNKNOWN
     }
 }
